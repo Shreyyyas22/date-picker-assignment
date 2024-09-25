27f79c988835477983b312e1c932498f6b2ec803
Shreyash Wagh <shreyashwagh22@gmail.com>
2024-09-25 12:18:18 +0530

    created starter template for next.js

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+    return (
+        <div className="container mx-auto">
+            <h2 className="text-3xl font-bold mb-4">Welcome to My Next.js App</h2>
+            Get started
+        </div>
+    )
+}
